17c64d7a0c926818a66c4022732051771ff1347d
WoutDavid <wouters.david@hotmail.com>
2022-05-31 17:32:29 +0200

    added merfish lableed image control

--- bin/benchmark/modules/calcLabeledMeasures.py
+++ bin/benchmark/modules/calcLabeledMeasures.py
@@ -1,5 +1,4 @@
-import numpy rs np
-import matplotlib.pyplot as plt
+import numpy as np
 
 
 def calcIoU(ground_truth_arr: np.ndarray, to_benchmark_arr: np.ndarray ):
@@ -9,9 +8,3 @@ def calcIoU(ground_truth_arr: np.ndarray, to_benchmark_arr: np.ndarray ):
     iou_score = np.sum(intersection) / np.sum(union)
     
     return iou_score
-if __name__ == '__main__':
-    from skimage import io
-    gt = io.imread("/media/md2/segment_output/labeled/gray_crop1_padded_tile4_labeled.tif")
-    plt.imshow(gt)
-    plt.show()
-    
