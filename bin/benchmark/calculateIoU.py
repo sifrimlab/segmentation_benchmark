79ea8ba25c198466431974eea65945a0f0aa1afa
WoutDavid <wouters.david@hotmail.com>
2022-05-31 09:45:41 +0200

    update IoU workflow

--- bin/benchmark/calculateIoU.py
+++ bin/benchmark/calculateIoU.py
@@ -4,12 +4,12 @@ import pandas as pd
 from modules.calcLabeledMeasures import calcIoU
 from skimage import io
 
-ground_truth = io.imread(sys.argv[1])
-labeled_image = io.imread(sys.argv[2])
-prefix = os.path.splitext(ground_truth)[0]
+labeled_image = io.imread(sys.argv[1])
+ground_truth = io.imread(sys.argv[2])
+prefix = os.path.splitext(sys.argv[2])[0]
 iou = calcIoU(ground_truth, labeled_image)
 
-df = pd.DataFrame({"name": prefix, "IoU": iou})
+df = pd.DataFrame({"name": prefix, "IoU": iou}, index = [0])
 df.to_csv(f"{prefix}_IoU.csv")
 
 
--- bin/benchmark/modules/calcLabeledMeasures.py
+++ bin/benchmark/modules/calcLabeledMeasures.py
@@ -3,8 +3,8 @@ import numpy as np
 
 def calcIoU(ground_truth_arr: np.ndarray, to_benchmark_arr: np.ndarray ):
     # IoU calculation
-    intersection = numpy.logical_and(ground_truth_arr, to_benchmark_arr)
-    union = numpy.logical_or(ground_truth_arr, to_benchmark_arr)
-    iou_score = numpy.sum(intersection) / numpy.sum(union)
+    intersection = np.logical_and(ground_truth_arr, to_benchmark_arr)
+    union = np.logical_or(ground_truth_arr, to_benchmark_arr)
+    iou_score = np.sum(intersection) / np.sum(union)
     
     return iou_score
